237441cc399d011213ff3d024951331f435ff522
losadev <losadauresdev@gmail.com>
2025-02-05 12:18:24 +0100

    Styles: add new resource button styles added

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import SearchBar from "../SearchBar/SearchBar"
 import './index.css'
+import Button from "../Button/Button"
 
 const MainContent = () => {
   return (
     <main>
+      <div className="container-input-btn">
       <SearchBar />
+      <Button />
+      </div>
       <Outlet />
     </main>
   )
